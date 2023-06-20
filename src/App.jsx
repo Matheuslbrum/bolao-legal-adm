5688c87f901599749c07b280ff270c3343e49bb8
MatheusLBrum <matheusdelimabrum@gmail.com>
2023-06-20 14:30:25 -0300

    creating project, installing and configuring libs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AdmForm from './components/AdmForm/AdmForm.jsx'
+import UserForm from './components/UserForm/UserForm.jsx'
+import ScoreForm from './components/ScoreForm/ScoreForm.jsx'
+
+function App() {
+  return (
+    <>
+      <AdmForm/>
+      <ScoreForm/>
+      <UserForm/>
+    </>
+  )
+}
+
+export default App
